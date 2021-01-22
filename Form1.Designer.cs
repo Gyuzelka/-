4a776c1c81db2e851cb53465e44cd047fa35e38a
Gyuzel Antonova <gyuzelantonova@bk.ru>
2021-01-22 14:16:14 +0300

    add programm

---
+++ Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(501, 91);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(525, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Выходная переменная нечеткой модели (состоит из термов):\r\n-Увеличение времени раб" +
+    "оты зеленого света (нет увеличения (Z), малое (S), среднее (M), большое L))\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(564, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(354, 234);
+            this.label3.TabIndex = 4;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 346);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1047, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = resources.GetString("label4.Text");
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(259, 365);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(34, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 372);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(192, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Кол-во машин на стороне прибытия:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 400);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(241, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Кол-во машин на стороне скопления очереди:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(259, 395);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(34, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 365);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 53);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "РАССЧЁТ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(389, 368);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(351, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Увеличение времени работы зеленого света на стороне прибытия:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(789, 365);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(29, 20);
+            this.textBox3.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(789, 393);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(29, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(959, 362);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(20, 56);
+            this.listBox1.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(389, 398);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(400, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Увеличение времени работы зеленого света на стороне скопления очереди:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(985, 365);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 53);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "ОЧИСТИТЬ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(836, 368);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(122, 39);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Выходная переменная\r\nнечеткой модели\r\nпринимает значения:";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WindowsFormsApp2.Properties.Resources._31;
+            this.pictureBox3.Location = new System.Drawing.Point(924, 68);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(138, 139);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsApp2.Properties.Resources._2;
+            this.pictureBox2.Location = new System.Drawing.Point(16, 239);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(254, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp2.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 107);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(240, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1065, 427);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Объявление необходимых переменных
+            int x, y, zn, temp, count=0;
+            double z=0.0;
+            char[] zz;
+            zz = new char[17];
+            //Задание входных переменных
+            x = Convert.ToInt32(textBox1.Text);
+            y = Convert.ToInt32(textBox2.Text);
+            //Определение выходной переменной на основании правил
+            if (x==y)
+            {
+                textBox3.Text = "0";
+                textBox4.Text = "0";
+            } else
+            if (x > y)
+            {
+                if (x < 2 && y < 2)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+                if (x < 2 && y >= 0 && y < 4)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+                if (x < 2 && y >= 2 && y < 6)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+                if (x < 2 && y >= 4)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+
+                if (x >= 0 && x < 4 && y < 2)
+                {
+                    count = count + 1;
+                    zz[count] = 'S';
+                }
+                if (x >= 0 && x < 4 && y >= 0 && y < 4)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+                if (x >= 0 && x < 4 && y >= 2 && y < 6)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+                if (x >= 0 && x < 4 && y >= 4)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+
+                if (x >= 2 && x < 6 && y < 2)
+                {
+                    count = count + 1;
+                    zz[count] = 'M';
+                }
+                if (x >= 2 && x < 6 && y >= 0 && y < 4)
+                {
+                    count = count + 1;
+                    zz[count] = 'S';
+                }
+                if (x >= 2 && x < 6 && y >= 2 && y < 6)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+                if (x >= 2 && x < 6 && y >= 4)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+
+                if (x >= 4 && y < 2)
+                {
+                    count = count + 1;
+                    zz[count] = 'L';
+                }
+                if (x >= 4 && y >= 0 && y < 4)
+                {
+                    count = count + 1;
+                    zz[count] = 'M';
+                }
+                if (x >= 4 && y >= 2 && y < 6)
+                {
+                    count = count + 1;
+                    zz[count] = 'S';
+                }
+                if (x >= 4 && y >= 4)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+
+                for (int i = 1; i <= count; i++)
+                {
+                    listBox1.Items.Add(Convert.ToString(zz[i]));
+                    if (zz[i] == 'Z')
+                    {
+                        zn = 0;
+                        z = z + zn;
+                    }
+                    if (zz[i] == 'S')
+                    {
+                        zn = 2;
+                        z = z + zn;
+                    }
+                    if (zz[i] == 'M')
+                    {
+                        zn = 4;
+                        z = z + zn;
+                    }
+                    if (zz[i] == 'L')
+                    {
+                        zn = 6;
+                        z = z + zn;
+                    }
+                }
+                z = z / count;
+                textBox3.Text = Convert.ToString(z);
+                textBox4.Text = "0";
+            } else
+            {
+                temp = x;
+                x = y;
+                y = temp;
+                if (x < 2 && y < 2)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+                if (x < 2 && y >= 0 && y < 4)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+                if (x < 2 && y >= 2 && y < 6)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+                if (x < 2 && y >= 4)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+
+                if (x >= 0 && x < 4 && y < 2)
+                {
+                    count = count + 1;
+                    zz[count] = 'S';
+                }
+                if (x >= 0 && x < 4 && y >= 0 && y < 4)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+                if (x >= 0 && x < 4 && y >= 2 && y < 6)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+                if (x >= 0 && x < 4 && y >= 4)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+
+                if (x >= 2 && x < 6 && y < 2)
+                {
+                    count = count + 1;
+                    zz[count] = 'M';
+                }
+                if (x >= 2 && x < 6 && y >= 0 && y < 4)
+                {
+                    count = count + 1;
+                    zz[count] = 'S';
+                }
+                if (x >= 2 && x < 6 && y >= 2 && y < 6)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+                if (x >= 2 && x < 6 && y >= 4)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+
+                if (x >= 4 && y < 2)
+                {
+                    count = count + 1;
+                    zz[count] = 'L';
+                }
+                if (x >= 4 && y >= 0 && y < 4)
+                {
+                    count = count + 1;
+                    zz[count] = 'M';
+                }
+                if (x >= 4 && y >= 2 && y < 6)
+                {
+                    count = count + 1;
+                    zz[count] = 'S';
+                }
+                if (x >= 4 && y >= 4)
+                {
+                    count = count + 1;
+                    zz[count] = 'Z';
+                }
+
+                for (int i = 1; i <= count; i++)
+                {
+                    listBox1.Items.Add(Convert.ToString(zz[i]));
+                    if (zz[i] == 'Z')
+                    {
+                        zn = 0;
+                        z = z + zn;
+                    }
+                    if (zz[i] == 'S')
+                    {
+                        zn = 2;
+                        z = z + zn;
+                    }
+                    if (zz[i] == 'M')
+                    {
+                        zn = 4;
+                        z = z + zn;
+                    }
+                    if (zz[i] == 'L')
+                    {
+                        zn = 6;
+                        z = z + zn;
+                    }
+                }
+                z = z / count;
+                textBox3.Text = "0";
+                textBox4.Text = Convert.ToString(z);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            int x, y, zn, temp, count = 0;
+            double z = 0.0;
+            char[] zz;
+            zz = new char[17];
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            listBox1.Items.Clear();
+        }
+    }
+}
